fdead30234b4912abe3e6d53ef21b381b5ac09e6
Randell G Smith <73455700+Catpockets@users.noreply.github.com>
2022-06-19 22:06:09 -0500

    Update stock.py

--- stock.py
+++ stock.py
@@ -22,3 +22,142 @@ if not tickerSymbol:
 else:
     st.header(f"Your ticker is {tickerSymbol}")
     
+    # Load animations from Lottie
+    def load_lottieurl(url):
+        r = requests.get(url)
+        if r.status_code != 200:
+            return None
+        return r.json()
+
+    lottie_coding = load_lottieurl("https://assets9.lottiefiles.com/packages/lf20_oAtVDo.json")
+
+    # Use local CSS
+    # def local_css(file_name):
+    #    with open(file_name) as f:
+    #        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+    #local_css("/style/style.css")
+
+    # Ticker data feed
+    tickerData = yf.Ticker(tickerSymbol)
+    tickerDf = tickerData.history(period='1d', start='2000-5-31', end=today)
+    tickers = [tickerSymbol]
+    longName = tickerData.info['longName']
+    numshares = tickerData.info['marketCap']
+    profitMargins = tickerData.info['profitMargins']
+    grossMargins = tickerData.info['grossMargins']
+    operatingCashflow = tickerData.info['operatingCashflow']
+    revenueGrowth = tickerData.info['revenueGrowth']
+    totalCash = tickerData.info['totalCash']
+    totalDebt = tickerData.info['totalDebt']
+    dayLow = tickerData.info['dayLow']
+    dayHigh = tickerData.info['dayHigh']
+    logourl = tickerData.info['logo_url']
+
+    for ticker in tickers:
+        ticker_yahoo = yf.Ticker(ticker)
+        data = ticker_yahoo.history()
+        last_close = data['Close'].iloc[-1]
+
+    for ticker in tickers:
+        ticker_yahoo = yf.Ticker(ticker)
+        data = ticker_yahoo.history()
+        last_open = data['Open'].iloc[-1]
+
+    float_open = f"${last_open:.2f}"
+    float_close = f"${last_close:.2f}"
+    percentchange = f"{last_close / last_open:.2f}%"
+
+    #dayLow dayHigh
+    today = date.today()
+
+    # Header Selection
+
+    with st.container():
+        st.write("---") # Dividert
+        st.subheader("Analyst: Randell G Smith :chart_with_upwards_trend:")
+        st.title("Ticker Stock analysis using Streamlit")
+        left_column, right_column = st.columns(2)
+        with left_column:
+            st.write("""
+            This is a test application to demo the power of streamlit as a viable python coding so
+            The live dataset is Yahoo Finance Python API (yfinance) and is a live datastream of st
+            This application attempts to connect to live data feeds and plot out stock market perf
+
+            This application will take user input in sidebar and automatically pull data from API 
+            """)
+
+        with right_column:
+            st_lottie(lottie_coding, height=250, key="coding")
+
+    col1, col2, col3 = st.columns(3)
+    col1.metric("Current Price", float_close, percentchange)
+
+    # Body Body
+    with st.container():
+        st.write("---") # Dividert
+        st.header("Ticker Information")
+        st.write(f"Name: {longName}")
+        st.image(logourl)
+        left_column, right_column = st.columns(2)
+        with left_column:
+
+            st.write(f"Last Close: {last_close:.2f}")
+            st.write(f"Day Low Trade: {dayLow}")
+            st.write(f"Day High Trade: {dayHigh}")
+            st.write(f"Market Cap: {numshares}")
+            st.write(f"Profit Margins: {profitMargins}")
+
+        with right_column:
+            st.write(f"Gross Margins: {grossMargins}")
+            st.write(f"Operating Cash Flow: {operatingCashflow}")
+            st.write(f"Total Cash: {totalCash}")
+            st.write(f"Total Debt: {totalDebt}") 
+
+    # Charts
+    with st.container():
+        left_column, right_column = st.columns(2)
+        with left_column:
+            st.line_chart(tickerDf.Close)
+        with right_column:
+            st.line_chart(tickerDf.Volume)
+
+    # Dataframe
+    st.header("RAW DATA")
+    dataf = tickerData.history(period="MAX")
+    st.write(dataf)
+    @st.cache
+    def convert_df(df):
+            # IMPORTANT: Cache the conversion to prevent computation on every rerun
+            return df.to_csv().encode('utf-8')
+
+    # csv = convert_df(dataf)
+    # 
+    # st.download_button(
+    #      label="Download data as CSV"
+    #      data=csv,
+    #      file_name='large_df.csv',
+    #      mime='text/csv',
+    #  )         
+
+    # Contact me
+    with st.container():
+        st.write("---")
+        st.header("Contact me!")
+        st.write("##")
+
+    # documentation: https://formsubmit.co/
+        contact_form = """
+        <form action="https://formsubmit.co/rgsmith787@gmail.com" method="POST">
+            <input type="hidden" name="_capcha" value="false">
+            <input type="text" name="name" placeholder="Your name" required>
+            <input type="email" name="email" placeholder="Your email" required>
+            <textarea name="message" placeholder="Your message here" required></textarea>
+            <button type="submit">Send</button>
+        </form>
+        """
+        left_column, right_column = st.columns(2)
+        with left_column:
+            st.markdown(contact_form, unsafe_allow_html=True)
+        with right_column:
+            st.empty()
