7f724c047eeca737a260ec376e739174635fd203
Randell G Smith <73455700+Catpockets@users.noreply.github.com>
2022-06-19 22:28:24 -0500

    Update stock.py

--- stock.py
+++ stock.py
@@ -32,11 +32,11 @@ else:
     lottie_coding = load_lottieurl("https://assets2.lottiefiles.com/packages/lf20_49rdyysj.json")
 
     # Use local CSS
-    def local_css(file_name):
-        with open(file_name) as f:
-            st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+    #def local_css(file_name):
+    #    with open(file_name) as f:
+    #        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-    local_css("/style/style.css")
+    #local_css("/style/style.css")
 
     # Ticker data feed
     tickerData = yf.Ticker(tickerSymbol)
