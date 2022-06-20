f69f7f4c893c93e1398abf1b34477637b82d4a80
Randell G Smith <73455700+Catpockets@users.noreply.github.com>
2022-06-19 23:03:18 -0500

    Update stock.py

--- stock.py
+++ stock.py
@@ -14,10 +14,11 @@ today = date.today()
 # Sidebar Setup
 st.sidebar.header("Options")
 my_form = st.sidebar.form(key = "form1")
+dateinput = my_form.date_input("Date test")
 tickerSymbol = my_form.text_input(label = "Enter ticker symbol")
 #startdate = my_form.date_input("Select Start date", datetime.date(2015, 1, 1))
 submit = my_form.form_submit_button(label = "Submit Symbol")
-dateinput = st.sidebar.date_input("Date test")
+
 if not tickerSymbol:
     st.header("Please enter a ticker symbol into the sidebar")
 else:
